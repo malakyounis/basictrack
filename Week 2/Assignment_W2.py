2a079d7a1d833d703956f70deafa44eb1dd2d92a
malakyounis <m.mohamed.7@student.rug.nl>
2020-09-24 17:23:41 +0200

    Week2- Assignment

--- Week 2/Assignment_W2.py
+++ Week 2/Assignment_W2.py
@@ -1,5 +1,46 @@
-transportation_mode = str(input("Are you traveling by bike, car, or walking?"))
-speed = int(input("How fast do you travel by" + transportation_mode + "in Kms"))
-distance = int(input("How far away is the place you're going? (in km's)"))
-time= distance/speed
+# Question1
+print("Question1")
+a = ("All ")
+b = ("work ")
+c = ("and ")
+d = ("no ")
+e = ("play ")
+f = ("makes ")
+g = ("Jack ")
+h = ("a ")
+i = ("dull ")
+j = ("boy.")
+print(a + b + c + d + e + f + g + h + i + j)
 
+# Question2
+print("Question2")
+print(6 * (1 - 2))
+
+# Question3
+# Comment included print(a+b+c+d+e+f+g+h+i+j)
+# Answer= The code is deactivated
+
+# Question4
+print("Question4")
+bruce = 6
+print(bruce + 4)
+
+# Question6
+print("Question6")
+print(5 % 2)
+print(9 % 5)
+print(15 % 12)
+print(12 % 15)
+print(6 % 6)
+print(0 % 7)
+# It gave me an error like I expected, because this is not a valid calculation. It is undefined.
+
+# Assignment Part 2
+print("Assignment- Part 2")
+transportation_mode = str(input("Are you traveling by bike, car, or walking?"))
+speed = int(input("How fast do you travel by " + transportation_mode + "? (in h/Kms)"))
+distance = int(input("How far away is the place you're going? (in km's)"))
+print(distance / speed)
+
+# I could not put it into the sentence below, it gave me an error
+# print("It will take you " + int((distance/speed)) + "to get to where you are going!")
--- debug_example.py
+++
@@ -1,4 +0,0 @@
-ask_user = int(input("Please give me a number"))
-
-print(ask_user + 2)
-
