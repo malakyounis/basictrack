897ca7b5754ce51d271743571da7e952fc254f04
malakyounis <m.mohamed.7@student.rug.nl>
2020-09-24 12:51:05 +0200

    Assignment, trial 1

--- Week 2/Assignment_W2.py
+++ Week 2/Assignment_W2.py
@@ -1,3 +1,5 @@
 transportation_mode = str(input("Are you traveling by bike, car, or walking?"))
 speed = int(input("How fast do you travel by" + transportation_mode + "in Kms"))
+distance = int(input("How far away is the place you're going? (in km's)"))
+time= distance/speed
 
